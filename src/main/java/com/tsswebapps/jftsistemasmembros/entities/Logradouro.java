a683d2a8d34bd24a5be57f980c62d3f9afb4a683
thiago-santanna <uzmaki_thiago@outlook.com>
2022-06-29 19:10:31 -0300

    classes de endereco

---
+++ src/main/java/com/tsswebapps/jftsistemasmembros/entities/Logradouro.java
@@ -0,0 +1,77 @@
+package com.tsswebapps.jftsistemasmembros.entities;
+
+public class Logradouro {
+	private String id;
+	private String cep;
+	private String logradouro;
+	private String bairro;
+	private String localidade;
+	private String ibge;
+	private String uf;
+	private Pais pais;
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getIbge() {
+		return ibge;
+	}
+
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+}
---
+++ src/main/java/com/tsswebapps/jftsistemasmembros/entities/Pais.java
@@ -0,0 +1,22 @@
+package com.tsswebapps.jftsistemasmembros.entities;
+
+public class Pais {
+	private String id;
+	private String descricao;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
